04616c4d27daf62633ed6476e5b7ca8cae61f732
adib3006 <ma.adib3006@gmail.com>
2022-09-29 00:48:00 +0600

    integration done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='home'>
+                <div className='activity-container'>
+                    <h1>Hello</h1>
+                </div>
+                <div className='info-container'>
+                    <h1>world</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
